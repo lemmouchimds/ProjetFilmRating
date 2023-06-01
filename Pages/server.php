3d04486858a05093d26b0f1ff0d32c4d3f958ff8
lemmouchimds <lemmouchimds@gmail.com>
2023-06-01 15:05:36 +0100

    Ca marche

--- Pages/server.php
+++ Pages/server.php
@@ -12,6 +12,18 @@ if (isset($_GET['titre']))
     //$query = "insert into films (titre, annee, rating, emotion, description, affciche, realisateur) values (?,?,?,?,?,?) ";
     
     $stmt = $link->prepare("INSERT INTO films (titre, affiche, emotion, annee, description, realisateur) VALUES (?, ?, ?, ?, ?, ?)");
+    
+    $titre = $_GET['titre'];
+    $affiche = $_GET['affiche'];
+    $emotion = $_GET['emotion'];
+    $annee = $_GET['annee'];
+    $description = $_GET['description'];
+    $realisateur = $_GET['realisateur'];
+
+    $stmt->bind_param("sssiss", $titre, $affiche, $emotion, $annee, $description, $realisateur);
+    $result = $stmt->execute();
+
+    header("Location: ../index.php");
     $stmt->bind_param("sssiss", $titre, $affiche, $emotion, $annee, $description, $realisateur);
     $result = $stmt->execute();
     header("Location: ../index.php");
