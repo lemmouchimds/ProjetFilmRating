e4f44120b033fb1311c8c8fff0f750f580be94cd
manyl2k3 <manylbouammar15@gmail.com>
2023-06-01 14:49:58 +0100

    encoreee

--- Pages/server.php
+++ Pages/server.php
@@ -7,8 +7,11 @@ $dbname = "films";
 if (isset($_GET['titre'])) 
 {
 
-    $query = "insert into films (titre, annee, rating, emotion, description, affciche, realisateur) values ($_GET['titre'], $_GET['annee'], $_GET['rating'], $_GET['emotion'], $_GET['description'], $_GET['affiche'], $_GET['realisateur'], $_GET['nbvisite']) ";
-    $result = mysqli_query($conn, $query);*
+    //$query = "insert into films (titre, annee, rating, emotion, description, affciche, realisateur) values (?,?,?,?,?,?) ";
+    
+    $stmt = $link->prepare("INSERT INTO films (titre, affiche, emotion , annee, description, realisateur) VALUES (?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("sss", $userName, $email, $password);
+    $result = mysqli_query($conn, $query);
     header("Location: ../index.php");
     if ($result) {
         echo "Data inserted";
