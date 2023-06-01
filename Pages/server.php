f635a8647b6c982adbadca5f816f978e74d1ca13
lemmouchimds <lemmouchimds@gmail.com>
2023-06-01 14:17:11 +0100

    ajouter encore

--- Pages/server.php
+++ Pages/server.php
@@ -8,7 +8,8 @@ if (isset($_GET['titre']))
 {
 
     $query = "insert into films (titre, annee, rating, emotion, description, affciche, realisateur) values ($_GET['titre'], $_GET['annee'], $_GET['rating'], $_GET['emotion'], $_GET['description'], $_GET['affiche'], $_GET['realisateur'], $_GET['nbvisite']) ";
-    $result = mysqli_query($conn, $query);
+    $result = mysqli_query($conn, $query);*
+    header("Location: ../index.php");
     if ($result) {
         echo "Data inserted";
     } else {
