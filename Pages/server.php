7736a24dec2b3d4897ecd57902fcd491de17e515
lemmouchimds <lemmouchimds@gmail.com>
2023-06-02 23:53:53 +0100

    you cant add the same movie twice

--- Pages/server.php
+++ Pages/server.php
@@ -9,26 +9,27 @@ $link = mysqli_connect($servername, $username, $password, $dbname);
 if (isset($_GET['titre'])) 
 {
 
-    $stmt = $link->prepare("INSERT INTO films (titre, affiche, emotion, année, description, realisateur) VALUES (?, ?, ?, ?, ?, ?)");
-      
-    
     $titre = $_GET['titre'];
-    $affiche = $_GET['affiche'];
-    $emotion = $_GET['emotion'];
-    $annee = $_GET['année'];
-    $description = $_GET['description'];
-    $realisateur = $_GET['realisateur'];
+    $query = "SELECT * FROM films WHERE titre = '$titre'";
+    $result = mysqli_query($link, $query);
+    $data = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
-    
-    $stmt->bind_param("sssiss", $titre, $affiche, $emotion, $annee, $description, $realisateur);
-    $result = $stmt->execute();
-    header("Location: ../index.php");
-    if ($result) {
-        echo "Data inserted";
-    } else {
-        echo "Data not inserted";
+    if (empty($data)) {
+        $stmt = $link->prepare("INSERT INTO films (titre, affiche, emotion, année, description, realisateur) VALUES (?, ?, ?, ?, ?, ?)");
+        
+        
+        $titre = $_GET['titre'];
+        $affiche = $_GET['affiche'];
+        $emotion = $_GET['emotion'];
+        $annee = $_GET['année'];
+        $description = $_GET['description'];
+        $realisateur = $_GET['realisateur'];
+        
+        
+        $stmt->bind_param("sssiss", $titre, $affiche, $emotion, $annee, $description, $realisateur);
+        $result = $stmt->execute();
     }
 
-
-    # code...
+    header("Location: ../index.php");
+    
 }
